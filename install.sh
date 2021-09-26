dceecbe99132f32031ab7ed09ee0f0ff54f14ea0
demo2099 <available3099@gmail.com>
2021-09-26 18:20:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -111,7 +111,7 @@ install_main(){
     start=$(date "+%s")
     mkdir -p /opt/sspanel && cd /opt/sspanel
     rm -f docker-compose.yml  
-    wget https://raw.githubusercontent.com/Baiyuetribe/ss-panel-v3-mod_Uim/dev/Docker/master/docker-compose.yml     
+    wget https://raw.githubusercontent.com/demo2099/sspanelInstall/main/master/docker-compose.yml     
     blue "配置文件获取成功"
     greenbg "首次启动会拉取镜像，国内速度比较慢，请耐心等待完成"
     docker-compose up -d
